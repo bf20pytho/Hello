ea64d1f46257a34714911d4a32c2d0b218815d5c
bf20pytho <94049018+bf20pytho@users.noreply.github.com>
2021-11-10 12:41:23 +0100

    Update ciao.py

--- ciao.py
+++ ciao.py
@@ -1,3 +1,9 @@
-x = 1
-if x == 1:
-  print("Linux is the best O.S. in the world")
+import tkinter as tk
+
+root = tk.Tk()
+label = tk.Label(root, text="Linux is the best O.S. in the world!")
+# Place the label on the root window
+label.pack()
+# Run the event loop
+root.mainloop()
+
